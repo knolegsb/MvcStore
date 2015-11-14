4e60863111605bf5403ff198f36b59304a8b79db
knolegsb <seon2you@hotmail.com>
2015-11-14 00:34:46 -0800

    added page

--- MvcStore.WebUI/Controllers/ProductController.cs
+++ MvcStore.WebUI/Controllers/ProductController.cs
@@ -10,6 +10,7 @@ namespace MvcStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository) {
             this.repository = productRepository;
@@ -21,9 +22,9 @@ namespace MvcStore.WebUI.Controllers
         //    return View();
         //}
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize));
         }
     }
 }
