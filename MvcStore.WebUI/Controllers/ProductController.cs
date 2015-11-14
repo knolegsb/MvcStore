0b46b8c8498d5acf97569c9df28603f250de1e33
knolegsb <seon2you@hotmail.com>
2015-11-13 21:48:55 -0800

    changed product controller

---
+++ MvcStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using MvcStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository) {
+            this.repository = productRepository;
+        }
+
+        // GET: Product
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
